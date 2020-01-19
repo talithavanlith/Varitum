d3ef23ea4d085e21a67e5d4e1eafb80976527e69
Matthew Dockerty <dockertyml@gmail.com>
2020-01-20 11:49:09 +1300

    Implement hacky player movement for testing

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float jumpHeight = 8;
+    private new Rigidbody2D rigidbody;
+    private bool grounded = true;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionStay2D(Collision2D collider)
+    {
+        RaycastHit2D[] hits;
+
+        //We raycast down 1 pixel from this position to check for a collider
+        Vector2 positionToCheck = transform.position;
+        hits = Physics2D.RaycastAll(positionToCheck, new Vector2(0, -1), 0.01f);
+
+        //if a collider was hit, we are grounded
+        if (hits.Length > 0)
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(Time.deltaTime * Input.GetAxis("Horizontal") * 6, 0);
+
+        if (Input.GetButton("Jump") && grounded)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpHeight);
+        }
+    }
+}
