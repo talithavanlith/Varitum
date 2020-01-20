8dcbe9a5831a90b4c530c7d36d63fe4a5d0f02d8
Yuyang Zou <zoude603@student.otago.ac.nz>
2020-01-20 16:08:01 +1300

    attach gun to player

--- Assets/player/Gun.cs
+++ Assets/player/Gun.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public object player;
+    public GameObject player;
+    private Transform playertransform;
     private float fireCD;
     public Transform gravityArcPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+         playertransform = player.transform;
     }
     
     private void FixedUpdate()
     {
         rotate_direction();
 
-        Vector3 player_location = Camera.main.WorldToScreenPoint();
-
+        //Vector3 player_location = Camera.main.WorldToScreenPoint(playertransform.position);
+        Vector3 p2 = playertransform.position;
+        //Debug.Log(p2);
+        
+        transform.position = p2 + new Vector3(0.6f,0.1f,0f);
 
 
         if (Input.GetMouseButtonDown(0))
@@ -36,7 +40,7 @@ public class Gun : MonoBehaviour
         Vector3 obj = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 direction = mouse - obj;
         direction.z = 0f;
-        Debug.Log(direction);
+        //Debug.Log(direction);
 
         transform.right = direction;
     }
