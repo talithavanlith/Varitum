357ad25c7ef2bbbe9aa257f9af44eb891904f20e
Yuyang Zou <zoude603@student.otago.ac.nz>
2020-01-20 14:55:21 +1300

    new sprit: changed mouse icon to aim #to be added to game master

---
+++ Assets/player/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+       // fireCD += Time.fixedDeltaTime;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 target = Input.mousePosition;
+
+        }
+
+
+    }
+
+
+    private void fire (Vector3 gunpoition, Vector3 target)
+    {
+
+
+
+
+    }
+}
---
+++ Assets/player/change_mouse_icon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class change_mouse_icon : MonoBehaviour
+{
+    
+   
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    private void Start()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+}
