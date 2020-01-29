3ff9627ccc49bebd1e89adf9f25c61c6d2e2cf2b
Rokunamatata-DK <54697500+Rokunamatata-DK@users.noreply.github.com>
2020-01-30 01:25:22 +1300

    idel to running animation

--- Assets/player/Animations.cs
+++ Assets/player/Animations.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class Animations : MonoBehaviour
 {
-    private bool faceright = true;
-
+   
+    //public 
     public GameObject player;
     public GameObject arm;
+    public GameObject mainbody;
+    //local data fields
+    private bool faceright = true;
     private Transform arm_t;
     private Transform player_t;
     private float fireCD;
-    public Transform gravityArcPrefab;
+    private Animator anim;
+   // public Transform gravityArcPrefab;
+
 
 
     // Start is called before the first frame update
@@ -19,6 +24,7 @@ public class Animations : MonoBehaviour
     {
         player_t = player.transform;
         arm_t = arm.transform;
+        anim = mainbody.GetComponent<Animator>();
         //  arm_rotate_direction_right();
 
     }
@@ -31,6 +37,21 @@ public class Animations : MonoBehaviour
         arm_rotate_direction_right();
         Debug.DrawLine(arm_t.position, Camera.main.WorldToScreenPoint(transform.position), Color.red);
         Debug.DrawLine(arm_t.position, Camera.main.ScreenToWorldPoint( Input.mousePosition), Color.green);
+        //play animation
+
+      float  moveHorizontal = Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
+
+        if (moveHorizontal != 0)
+        {
+            anim.SetBool("isRunning",true);
+            Debug.Log("running");
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+
     }
     /*private void OnDrawGizmos()
     {
@@ -38,10 +59,10 @@ public class Animations : MonoBehaviour
     }*/
     private void arm_rotate_direction_right()
     {
-        Vector3 p2 = player_t.position;
+        //Vector3 p2 = player_t.position;
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector3 obj = Camera.main.WorldToScreenPoint(arm_t.position);
+       // Vector3 obj = Camera.main.WorldToScreenPoint(arm_t.position);
         Vector3 direction = mouse - arm_t.position;
         direction.z = 0f;
         //Debug.Log(direction);
