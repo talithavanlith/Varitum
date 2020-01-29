beb52ae64a08bef30abec7ae7ca94229748f251f
Rokunamatata-DK <54697500+Rokunamatata-DK@users.noreply.github.com>
2020-01-29 13:43:22 +1300

    player horizontal move tuned

--- Assets/player/gun.cs
+++ Assets/player/gun.cs
@@ -10,13 +10,23 @@ public class gun : MonoBehaviour
     private float moveHorizontal;
     private float moveVertical;
     private Vector2 movement;
-    public float speed = 1.2f;
+    //public value
+    public float acceleration = 1.2f;
+    public float maxspeed = 10f;
+    public float jumpforce = 5f;
+    public bool isGrounded = false;
+
+    [Range(0, .3f)] [SerializeField] private float MovementSmoothing = .05f;
+    private Vector3 zero_Velocity = Vector3.zero;
+    //testing
+
+
     // Start is called before the first frame update
     void Start()
     {
         //initialize
         rigibody = GetComponent<Rigidbody2D>();
-
+        rigibody.collisionDetectionMode=CollisionDetectionMode2D.Continuous;
         
     }
 
@@ -27,18 +37,31 @@ public class gun : MonoBehaviour
         moveHorizontal= Input.GetKey(KeyCode.D) ? 1 : Input.GetKey(KeyCode.A) ? -1 : 0;
         movement = new Vector2(moveHorizontal,0f);
 
+        //jump
+        //jump if isgrounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigibody.AddForce(new Vector2(0f, jumpforce), ForceMode2D.Impulse);
+        }
     }
     private void FixedUpdate()
     {
-        if (rigibody.velocity.x<0 && rigibody.velocity.x>-6)
+
+        Vector3 targetVelocity = new Vector2(moveHorizontal*maxspeed,rigibody.velocity.y);
+
+        rigibody.velocity = Vector3.SmoothDamp(rigibody.velocity,targetVelocity,ref zero_Velocity,MovementSmoothing);
+
+        //horizontal move
+       /* if (rigibody.velocity.x<0 && rigibody.velocity.x>-6)
         {
-            rigibody.AddForce(movement*20f*speed);
+            rigibody.AddForce(movement*20f*acceleration);
         }else if (rigibody.velocity.x < 6 && rigibody.velocity.x > 0)
         {
-            rigibody.AddForce(movement * 20f * speed);
-        }
+            rigibody.AddForce(movement * 20f * acceleration);
+        }*/
 
+        
 
-        rigibody.AddForce(movement*10F*speed);
+      //  rigibody.AddForce(movement*10F*speed);
     }
 }
