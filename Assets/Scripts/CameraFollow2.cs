529c0e79ad01cda7934fa88acf6a4b0d719799f6
Dominic <akatsuki1091@gmail.com>
2020-01-30 14:16:55 +1300

    no message

--- Assets/Scripts/CameraFollow2.cs
+++ Assets/Scripts/CameraFollow2.cs
@@ -25,7 +25,7 @@ public class CameraFollow2 : MonoBehaviour
         temp.x = playerTransform.position.x;
 
         temp.x += offset;
-        temp.y = playerTransform.position.y+2;
+        temp.y = playerTransform.position.y+1;
         temp.y += offset; 
 
 
