75f9313065449ee42aa01eb45f8243de75059937
Dominic <akatsuki1091@gmail.com>
2020-01-30 15:54:43 +1300

    Created a canvas and created a paralax prefab

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+
+    // Reference to UI panel that is our pause menu
+    public GameObject pauseMenuPanel;
+    // Reference to panel's script object 
+    PauseMenuManager pauseMenu;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Initialise the reference to the script object, which is a
+        // component of the pause menu panel game object
+        pauseMenu = pauseMenuPanel.GetComponent<PauseMenuManager>();
+        pauseMenu.Hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    if (Input.GetKey(KeyCode.Escape))
+        {
+            // If user presses ESC, show the pause menu in pause mode
+            pauseMenu.ShowPause();
+        }
+    }
+}
