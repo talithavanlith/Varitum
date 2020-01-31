b6bb0715a3e3fb8db9785fe44f684d99b9127c9e
Matthew Dockerty <dockertyml@gmail.com>
2020-01-31 14:17:26 +1300

    Make falling objects kill the player

---
+++ Assets/player/PlayerCrush.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCrush : MonoBehaviour
+{
+
+    public playerController player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("CRUSHED");
+        InanimateObject obj = collision.gameObject.GetComponent<InanimateObject>();
+        if (obj && obj.IsFalling())
+        {
+            player.die();
+            Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>());
+        }
+    }
+
+   
+}
