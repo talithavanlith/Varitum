c04c0d3b20ba580356e44cfeb8ec0cf8b80e7393
Dominic <akatsuki1091@gmail.com>
2020-02-13 11:03:47 +1300

    no message

---
+++ Assets/Scripts/SpaceShipLiftOff.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShipLiftOff : MonoBehaviour
+{
+    private GameObject spaceShip;
+    private Vector3 coordinates;
+    private GameObject[] flames;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spaceShip = GameObject.FindGameObjectWithTag("SpaceShip");
+        flames = GameObject.FindGameObjectsWithTag("shipFlame");
+        coordinates = spaceShip.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coordinates = spaceShip.transform.position;
+        spaceShip.transform.SetPositionAndRotation(new Vector3(coordinates.x, coordinates.y + 1, coordinates.z), spaceShip.transform.rotation);
+        for(int i= 0; i<flames.Length; i++)
+        {
+            Debug.Log("wtf");
+            flames[i].transform.SetPositionAndRotation(new Vector3(flames[i].transform.position.x, coordinates.y-19, flames[i].transform.position.z-4), spaceShip.transform.rotation);
+        }
+    }
+}
